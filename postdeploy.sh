5bbde9d062d52647810044ef567738970c4e170f
Deepak Bharti <deepakbharti823@gmail.com>
2018-09-04 11:32:15 +0530

    postdeploy script modified

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,5 +1,6 @@
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('deepak', 'admin@example.com', 'cooladmins@3')" | python manage.py shell;
 echo  "python manage.py migrate && python manage.py makemigrations" | python manage.py shell;
+echo  "python manage.py migrate && python manage.py migrate" | python manage.py shell;
 echo  "python manage.py collectstatic" | python manage.py shell
 
 
