1e94adb6b14bd9febfeb79d28ed567107881a6ee
Deepak Bharti <deepakbharti823@gmail.com>
2018-09-04 14:44:27 +0530

    Heroku deployment related changes

--- ContriHub/settings.py
+++ ContriHub/settings.py
@@ -102,12 +102,6 @@ if 'DATABASE_URL' in os.environ:
     import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
 else:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #     }
-    # }
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -118,6 +112,12 @@ else:
             'PORT': '',
         }
     }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     }
+    # }
 
 
 # Password validation
