8e01ee9e0fe8710286d9611762072bfb5f738ecc
Deepak Bharti <deepakbharti823@gmail.com>
2018-09-04 09:58:54 +0530

    preparing for deployment

--- ContriHub/wsgi.py
+++ ContriHub/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
+import os     
 
 from django.core.wsgi import get_wsgi_application
+#added for heroku
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ContriHub.settings')
 
 application = get_wsgi_application()
+#added for heroku
+application = DjangoWhiteNoise(application)
