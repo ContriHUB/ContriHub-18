3b409b839012e508b585a621c273b55389a8a0e9
unknown <ojha.ashwini.1998@gmail.com>
2018-10-26 23:42:30 +0530

    #97 Upvote and downvote feature added

--- ContriHub/urls.py
+++ ContriHub/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static  
 from django.contrib.auth.views import (LoginView, LogoutView)
 
-from Projects.views import (home,request_pr,response_pr,leaderboard,profile,remove_issue,remove_pr,contri_user)
+from Projects.views import (home,request_pr,response_pr,leaderboard,profile,remove_issue,remove_pr,contri_user,vote)
 from Users.views import (signin,register)
 from django.contrib.auth import views as auth_views
 
@@ -40,6 +40,7 @@ urlpatterns = [
     path('leaderboard', leaderboard , name='leaderboard'),     
     path('remove_issue', remove_issue , name='remove_issue'),
     path('remove_pr', remove_pr , name='remove_pr'),
+    path('vote', vote, name='vote'),
     #put such urls always after all
     path('<username>', profile, name='profile'),
     path('contributions/<username>', contri_user, name='contri_user'),
