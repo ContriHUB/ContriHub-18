61bd141e5ed161341385dcede60ad86dbc8cc96c
Deepak Bharti <deepakbharti823@gmail.com>
2018-10-27 01:17:23 +0530

    views modified

--- Projects/views.py
+++ Projects/views.py
@@ -69,7 +69,6 @@ def home(request):
                 project_filter = Issues.objects.filter(title_project=str(val))
                 mentor_filter  = Issues.objects.filter(mentor__username=str(val))
                 issues = project_filter or mentor_filter
-                
         else:
             print("all issues|else block")
             issues = Issues.objects.all()
